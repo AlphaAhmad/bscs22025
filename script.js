aef7d4e843369bc08a15f1fa5dcc2da3785cf697
Muhammad Ahmad <muhammadahmad8063@gmail.com>
2024-09-15 00:20:51 +0500

    70n percent progress complete

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// code for index.html
 document.getElementById("projectPage-nav").addEventListener("click", function() {
-    window.location.href = "linked-file.html"; // Replace with your file name
+    window.location.href = "project.html"; // Replace with your file name
 });
 
 document.getElementById("achievePage-nav").addEventListener("click", function() {
-    window.location.href = "linked-file.html"; // Replace with your file name
+    window.location.href = "achievement.html"; // Replace with your file name
 });
+
