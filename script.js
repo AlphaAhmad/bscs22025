58001b06dcf11e260007dc589708a35d59ec57aa
Muhammad Ahmad <muhammadahmad8063@gmail.com>
2024-09-14 15:09:16 +0500

    added buttons and javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("projectPage-nav").addEventListener("click", function() {
+    window.location.href = "linked-file.html"; // Replace with your file name
+});
+
+document.getElementById("achievePage-nav").addEventListener("click", function() {
+    window.location.href = "linked-file.html"; // Replace with your file name
+});
